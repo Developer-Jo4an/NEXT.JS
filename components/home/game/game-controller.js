061ce485ecedbb313c88da6fcaa21de680e255d4
Developer-Jo4an <107997257+Developer-Jo4an@users.noreply.github.com>
2024-03-21 14:34:52 +0300

    feat(Ball): создан отдельный класс для ball

--- components/home/game/game-controller.js
+++ components/home/game/game-controller.js
@@ -1,7 +1,7 @@
 import { Assets, Container, Sprite } from 'pixi.js'
-import { gsap } from 'gsap'
 import ball from '@/public/assets/ball/ball.png'
 import background from '@/public/assets/background/background.png'
+import { Ball } from '@/components/home/game/sprites/Ball'
 
 export default class GameController {
     static BALL = 'ball'
@@ -17,18 +17,20 @@ export default class GameController {
         this.app = app
 		this.$container = container
 		this.gameArea = null
-		this.background = null
 	    this.ball = null
     }
 
 	async activateController() {
-		if (!this.app || this.$container.children.length === 2) return
-		const assetsArray = [{ alias: 'ball', src: ball }, { alias: 'background', src: background } ]
+		const assetsArray = [
+			{ alias: 'ball', src: ball },
+			{ alias: 'background', src: background }
+		]
 
-		await Promise.all([this.app.init({ width: this.$container.offsetWidth, height: this.$container.offsetHeight }), Assets.load(assetsArray)])
+		await this.app.init({ width: this.$container.offsetWidth,  height: this.$container.offsetHeight })
+
+		await Assets.load(assetsArray)
 
 		this.addGameArea()
-		this.addBackground()
 		this.addBall()
 		this.attach()
 
@@ -36,125 +38,65 @@ export default class GameController {
 	}
 
 	addGameArea() {
-		this.gameArea = new Container()
-
-		this.gameArea.x = (this.app.screen.width - this.gameArea.width) / 2
-		this.gameArea.y = (this.app.screen.height - this.gameArea.height) / 2
-
-		this.app.stage.addChild(this.gameArea)
-	}
-
-	addBackground() {
 		const background = Sprite.from(GameController.BACKGROUND)
 
 		background.anchor.set(0.5)
-
 		background.width = +this.$container.offsetWidth
 		background.height = +this.$container.offsetHeight
-		background.ratio = background.width / background.height
 
-		this.background = background
+		this.gameArea = new Container()
+
+		this.gameArea.x = this.$container.offsetWidth / 2
+		this.gameArea.y = this.$container.offsetHeight / 2
+		this.gameArea.ratio = background.width / background.height
 
 		this.gameArea.addChild(background)
+		this.app.stage.addChild(this.gameArea)
 	}
 
     addBall() {
-	    this.ball = Sprite.from(GameController.BALL)
+		this.ball = Ball.ballFrom(GameController.BALL)
 
-	    this.ball.isAnimation = false
-
-	    this.ball.anchor.set(0.5)
-
-	    this.ball.width = this.gameArea.width / 15
-	    this.ball.height = this.gameArea.width / 15
+		this.ball.width = this.gameArea.width / 15
+		this.ball.height = this.gameArea.width / 15
 
 		this.ball.y = (this.gameArea.height / 2) - this.ball.width
 
-	    this.ball.interactive = true
-	    this.ball.buttonMode = true
+		this.gameArea.addChild(this.ball)
+	}
 
-        this.gameArea.addChild(this.ball)
-    }
-
-    ballEventClick() {
-	    if (!this.ball.isAnimation) {
-		    this.ball.isAnimation = true
-
-		    let bounceTimeline = gsap.timeline({
-			    onComplete: () => {
-				    this.ball.isAnimation = false
-				    this.ball.emit(GameController.BALL_DROPPED)
-			    }
-		    })
-
-			const currentState = {
-				scaleY: this.ball.scale.y,
-				scaleX: this.ball.scale.x,
-				y: this.ball.y
-			}
-
-		    for (let i = 0; i < 12; i++) {
-			    let bounceHeight = this.gameArea.height / 2 / (i + 1)
-			    let bounceDuration = 0.5 / (i + 1)
-
-			    bounceTimeline
-				.to(this.ball, {
-				    y: this.ball.y - bounceHeight,
-				    duration: bounceDuration,
-				    ease: 'power2.out',
-					onStart: () => {
-						gsap.to(this.ball.scale, { x: currentState.scaleX * 0.9, y: currentState.scaleY * 1.1, duration: bounceDuration })
-					},
-			    })
-			    .to(this.ball, {
-				    y: this.ball.y + this.ball.height * 0.11,
-				    duration: bounceDuration,
-				    ease: 'power2.in',
-					onStart: () => {
-						gsap.to(this.ball.scale, { x: currentState.scaleX * 1.1, y: currentState.scaleY * 0.9, duration: bounceDuration })
-					},
-				    onComplete: () => {
-					    gsap.to(this.ball.scale, { x: currentState.scaleX, y: currentState.scaleY, duration: 0.1 })
-				    }
-			    })
-		    }
-			bounceTimeline.to(this.ball, { y: currentState.y, duration: 0.1 })
-	    }
-
-    }
+    ballEventClick() { this.ball.animation(this.gameArea, GameController.BALL_DROPPED) }
 
 	resizeApp() {
 		const width = +this.$container.offsetWidth
 		const height = +this.$container.offsetHeight
+		const areaWidth = this.gameArea.width
+		const areaHeight = this.gameArea.height
 
 		this.app.renderer.resize(width, height)
 
-
-		if (width < this.gameArea.width) {
+		if (width < areaWidth) {
 			this.gameArea.width = width
-			this.gameArea.height = width / this.background.ratio
+			this.gameArea.height = width / this.gameArea.ratio
 		}
 
-		if (height < this.gameArea.height) {
+		if (height < areaHeight) {
 			this.gameArea.height = height
-			this.gameArea.width = height * this.background.ratio
+			this.gameArea.width = height * this.gameArea.ratio
 		}
 
-		if (width > this.gameArea.width && (height > this.gameArea.height)) {
+		if (width > areaWidth && (height > areaHeight)) {
 			this.gameArea.width = width
-			this.gameArea.height = width / this.background.ratio
+			this.gameArea.height = width / this.gameArea.ratio
 		}
 
-		if (height > this.gameArea.height && (width > this.gameArea.width)) {
+		if (height > areaHeight && (width > areaWidth)) {
 			this.gameArea.height = height
-			this.gameArea.width = height * this.background.ratio
+			this.gameArea.width = height * this.gameArea.ratio
 		}
 
 		this.gameArea.x = 0.5 * (width - this.gameArea.width) + (this.gameArea.width / 2)
 		this.gameArea.y = 0.5 * (height - this.gameArea.height) + (this.gameArea.height / 2)
-
-		this.ball.width = this.gameArea.width < 1500 ? 100 : this.gameArea.width / 15
-		this.ball.height = this.gameArea.width < 1500 ? 100 : this.gameArea.width / 15
 	}
 
 	attach() {
---
+++ components/home/game/sprites/Ball.js
@@ -0,0 +1,84 @@
+import { Sprite } from 'pixi.js'
+import { gsap } from 'gsap'
+
+export class Ball extends Sprite {
+    constructor(texture) {
+        super(texture)
+        this.isAnimation = false
+    }
+
+    animation(area, emitter) {
+        if (!this.isAnimation) {
+            this.isAnimation = true
+
+            let bounceTimeline = gsap.timeline({
+                onComplete: () => {
+                    this.isAnimation = false
+                    this.emit(emitter)
+                }
+            })
+
+            const currentState = {
+                scaleY: this.scale.y,
+                scaleX: this.scale.x,
+                y: this.y
+            }
+
+            const reductionMultiplier = 0.9
+            const magnificationMultiplier = 1.1
+
+            for (let i = 0; i < 12; i++) {
+                let bounceHeight = area.height / 2 / (i + 1)
+                let bounceDuration = 0.5 / (i + 1)
+
+                bounceTimeline
+                .to(this, {
+                    y: this.y - bounceHeight,
+                    duration: bounceDuration,
+                    ease: 'power2.out',
+                    onStart: () => {
+                        gsap.to(this.scale, {
+                            x: currentState.scaleX * reductionMultiplier,
+                            y: currentState.scaleY * magnificationMultiplier,
+                            duration: bounceDuration
+                        })
+                    },
+                })
+                .to(this, {
+                    y: this.y + this.height * magnificationMultiplier * 0.1,
+                    duration: bounceDuration,
+                    ease: 'power2.in',
+                    onStart: () => {
+                        gsap.to(this.scale, {
+                            x: currentState.scaleX * magnificationMultiplier,
+                            y: currentState.scaleY * reductionMultiplier,
+                            duration: bounceDuration
+                        })
+                    },
+                    onComplete: () => {
+                        gsap.to(this.scale, {
+                            x: currentState.scaleX,
+                            y: currentState.scaleY,
+                            duration: 0.1
+                        })
+                    }
+                })
+            }
+            bounceTimeline.to(this, {
+                y: currentState.y,
+                duration: 0.1
+            })
+        }
+    }
+
+    static ballFrom(texture) {
+        const sprite = super.from(texture)
+
+        sprite.anchor.set(0.5)
+
+        sprite.interactive = true
+        sprite.buttonMode = true
+
+        return Object.assign(new Ball(), sprite)
+    }
+}
